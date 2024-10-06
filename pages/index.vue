29cf2fcb29ef6fc325c18e261559e50e9797dcec
Saad-Khan-11 <saadkhan11632@outlook.com>
2024-10-06 18:00:15 +0500

    finally got the background image right

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,32 @@
 <template>
   <div class="font-Montserrat relative">
-    <div
-      class="absolute inset-0 bg-cover bg-center opacity-50"
-      style="background-image: url('../assets/images/home-bg.jpg')"
-    ></div>
-    <div class="relative min-h-[60vh] flex justify-between">
-      <div class="mt-40 ml-12 z-10">
-        <div class="text-5xl font-bold my-2">
-          Green Recipes for Anyone, Anywhere.
-        </div>
-        <div class="text-2xl font-semibold my-2">
-          We're called a recipe haven for a reason.
-        </div>
-        <div class="my-2">
-          Like I said, we're just a bunch of recipes. Green, healthy ones that
-          your body will like.
-        </div>
-        <Button class="fit-content" title="View Recipes"></Button>
+    <div>
+      <div
+        class="absolute inset-0 bg-cover bg-center opacity-50 bg-[url('../assets/images/home-bg.jpg')] z-5"
+      >
+        <img
+          src="../assets/images/home-bg.jpg"
+          alt=""
+          class="h-full w-full object-cover"
+        />
       </div>
-      <div class="object-cover h-[50vh] w-[50vw] z-10">
-        <img src="../assets/images/showcase-image.jpg" alt="my hero" />
+      <div class="relative min-h-[60vh] flex justify-between">
+        <div class="mt-40 ml-12">
+          <div class="text-5xl font-bold my-2">
+            Green Recipes for Anyone, Anywhere.
+          </div>
+          <div class="text-2xl font-semibold my-2">
+            We're called a recipe haven for a reason.
+          </div>
+          <div class="my-2">
+            Like I said, we're just a bunch of recipes. Green, healthy ones that
+            your body will like.
+          </div>
+          <Button class="fit-content" title="View Recipes"></Button>
+        </div>
+        <div class="object-cover h-[30vh] w-[30vw]">
+          <img src="../assets/images/showcase-image.jpg" alt="my hero" />
+        </div>
       </div>
     </div>
   </div>
