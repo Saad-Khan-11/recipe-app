ac8f32e8254f2d1e1bc0ce6be3bb2fd8ee1f15ff
Saad-Khan-11 <saadkhan11632@outlook.com>
2024-10-07 19:13:24 +0500

    created contact us page

--- pages/contact.vue
+++ pages/contact.vue
@@ -8,23 +8,31 @@
           class="h-full w-full object-cover"
         />
       </div>
-      <div class="relative min-h-[70vh] flex justify-between">
-        <div class="mt-40 ml-12">
-          <div class="text-6xl font-bold my-2">
-            Green Recipes for Anyone, Anywhere.
+      <div class="relative min-h-[70vh] flex justify-center text-center">
+        <div class="mt-40">
+          <div class="text-6xl font-bold">Got Any Questions? Ask Here!</div>
+          <div
+            class="flex flex-col items-center bg-green-400 rounded-lg my-12 p-4"
+          >
+            <div class="text-2xl my-2">
+              Our Support Staff replies within 24 hours.
+            </div>
+            <input
+              type="text"
+              class="w-full p-2 border-2 border-green-500 rounded-lg my-2"
+              placeholder="Name"
+            />
+            <input
+              type="text"
+              class="w-full p-2 border-2 border-green-500 rounded-lg my-2"
+              placeholder="Email"
+            />
+            <textarea
+              class="w-full p-2 border-2 border-green-500 rounded-lg my-2 h-36"
+              placeholder="Type your message..."
+            />
+            <Button title="Send Message" class="text-xl px-6 py-2"></Button>
           </div>
-          <div class="text-2xl my-2">
-            Like I said, we're just a bunch of recipes. Green, healthy ones that
-            your body will like. And it won't cost you anything.
-          </div>
-          <Button title="View Recipes"></Button>
-        </div>
-        <div class="object-cover h-1/2 w-1/2 p-10">
-          <img
-            src="@/assets/images/showcase-image.jpg"
-            alt="my hero"
-            class="border-4 border-green-500 rounded-xl"
-          />
         </div>
       </div>
     </div>
