2ef5722a207cfc7a6ad197f4b12137f304ac8a54
Saad-Khan-11 <saadkhan11632@outlook.com>
2024-10-09 20:44:50 +0500

    trying to make a card component

--- components/Card.vue
+++ components/Card.vue
@@ -1,7 +1,11 @@
 <template>
-  <div></div>
+  <div class="card">Hello</div>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+.card {
+  @apply bg-green-500 hover:bg-green-700 duration-200 text-white font-bold rounded-lg;
+}
+</style>
---
+++ pages/cardbox.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card text-center">Cardbox</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.card {
+  @apply font-bold rounded-lg p-3 bg-slate-50 shadow-lg h-full m-8;
+}
+</style>
