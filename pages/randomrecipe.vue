f304e0e00afc9ad09a39b50a07e24c68ed8ef77c
Saad-Khan-11 <saadkhan11632@outlook.com>
2024-11-24 04:35:30 -0800

    cutting down unnecessary pages

--- pages/randomrecipe.vue
+++ pages/randomrecipe.vue
@@ -1,30 +1,29 @@
-<!-- The code below is AI written code. Need to study this and replicate it with my own logic -->
-
 <template>
-  <div>
-    <button @click="fetchRecipe">Get Random Recipe</button>
-    <div v-if="spoon && spoon.recipes && spoon.recipes.length > 0">
-      <h1>{{ spoon.recipes[0].title }}</h1>
-      <p v-html="spoon.recipes[0].summary"></p>
+  <div class="flex justify-center flex-col items-center text-center">
+    <div
+      v-if="spoon && spoon.recipes && spoon.recipes.length > 0"
+      class="flex justify-center flex-col items-center text-center"
+    >
+      <h1 class="mb-6 mt-12 text-xl font-bold">{{ spoon.recipes[0].title }}</h1>
+      <img :src="spoon.recipes[0].image" alt="" class="my-6" />
+      <p v-html="spoon.recipes[0].summary" class="my-6 w-3/4"></p>
     </div>
-    <div v-if="error">You have run into an unexpected error.</div>
-    <div v-else-if="loading">Loading...</div>
+    <div class="my-12 text-xl font-bold">
+      Feeling Lucky? Click the Button below!
+    </div>
+    <Button
+      @click="fetchRecipe"
+      class="p-4 m-4"
+      title="Get a Random Recipe"
+    ></Button>
   </div>
 </template>
 
 <script setup>
 const config = useRuntimeConfig();
 
-// Define reactive state variables
 const spoon = ref(null);
-const error = ref(false);
-const loading = ref(false);
-
-// Fetch function to retrieve recipe
 const fetchRecipe = async () => {
-  loading.value = true;
-  error.value = false;
-
   try {
     const response = await useFetch(
       "https://api.spoonacular.com/recipes/random",
@@ -35,14 +34,9 @@ const fetchRecipe = async () => {
         },
       }
     );
-
-    // Assuming response data is under `data.value` and recipes are in `recipes`
-    spoon.value = response.data?.value;
-    loading.value = false;
+    spoon.value = response.data.value;
   } catch (err) {
     console.error("Error fetching recipe:", err);
-    error.value = true;
-    loading.value = false;
   }
 };
 </script>
