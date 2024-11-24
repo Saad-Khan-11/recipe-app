ffd12be34ae9f46e21d6a53fe6c0e65afa05a85b
Saad-Khan-11 <saadkhan11632@outlook.com>
2024-11-24 05:37:12 -0800

    made SOME PROGRESS FINALYL!

---
+++ pages/sandbox/spoonapi.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="font-Montserrat relative flex flex-col text-center p-10">
+    <div class="text-6xl font-bold">Spoonacular API</div>
+    <div class="text-2xl">This is a sandbox page for the Spoonacular API</div>
+  </div>
+  <div>
+    <div class="flex justify-center">
+      <div class="text-xl mb-6">Recipe</div>
+      <div class="grid grid-cols-4">
+        <!-- <div v-for="r in response.data.recipes"></div>
+        <Card :recipes="r"></Card> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig();
+
+const spoon = ref(null);
+
+const response = await useFetch("https://api.spoonacular.com/recipes/random", {
+  params: {
+    apiKey: config.public.spoonApiKey,
+    number: 1,
+  },
+});
+
+spoon.value = response.data.value;
+
+console.log(spoon.value.recipes[0]);
+</script>
+
+<style scoped></style>
