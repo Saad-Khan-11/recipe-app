9cd80f207d41b3b86f5849f065e2b831a9509655
Saad-Khan-11 <saadkhan11632@outlook.com>
2025-01-29 18:39:56 +0500

    removed the usefetch function from recipes grid list section

--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -2,12 +2,6 @@
   <div class="font-Montserrat">
     <div class="flex flex-col justify-center">
       <p class="flex font-bold text-5xl p-12 justify-center">Recipes</p>
-      <Button
-        @click="fetchRecipe"
-        title="Refresh List"
-        class="p-4 max-w-fit mx-auto"
-      >
-      </Button>
     </div>
     <div class="grid grid-cols-4 gap-5 p-4 pb-16">
       <div v-if="spoon.length === 0">No recipes found.</div>
@@ -22,23 +16,13 @@
 const config = useRuntimeConfig();
 const spoon = ref([]);
 
-const fetchRecipe = async () => {
-  try {
-    const response = await $fetch(
-      "https://api.spoonacular.com/recipes/random",
-      {
-        params: {
-          apiKey: config.public.SPOON_API_KEY,
-          number: 16,
-        },
-      }
-    );
-    spoon.value = response.recipes || [];
-  } catch (err) {
-    console.error("Error fetching recipes:", err);
-  }
-};
-fetchRecipe();
+const response = await $fetch("https://api.spoonacular.com/recipes/random", {
+  params: {
+    apiKey: config.public.SPOON_API_KEY,
+    number: 16,
+  },
+});
+spoon.value = response.recipes || [];
 </script>
 
 <style scoped></style>
